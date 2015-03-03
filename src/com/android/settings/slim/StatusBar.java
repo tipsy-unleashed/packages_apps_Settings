e061483bc0ff38d1d460d15fac536425fe1930ad
Martinusbe <martinusbe@gmail.com>
2015-03-03 13:32:11 +0530

    Hide unused msim icons on setting preference(1/2) for Vatsal Added a listPreference through which no-sim icons can be hidden or displayed as per the user's wish
    Change-Id: I9ffca4f4c394fb41d8d00ad550f06b4455b92350
    Signed-off-by: martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	res/values/cm_strings.xml
    	res/xml/status_bar_settings.xml
    	src/com/android/settings/cyanogenmod/StatusBarSettings.java

--- src/com/android/settings/slim/StatusBar.java
+++ src/com/android/settings/slim/StatusBar.java
@@ -27,6 +27,7 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
 
@@ -119,6 +120,10 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
         int networkArrows = Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUS_BAR_SHOW_NETWORK_ACTIVITY, 0);
         updateNetworkArrowsSummary(networkArrows);
+
+        if (TelephonyManager.getDefault().getPhoneCount() <= 1) {
+            removePreference(Settings.System.STATUS_BAR_MSIM_SHOW_EMPTY_ICONS);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
