7d8f1de1470ca96f13230da60f715024c76d9346
martinusbe <martinusbe@gmail.com>
2015-06-01 23:49:39 +0200

    tipsy settings
    Signed-off-by: martinusbe <martinusbe@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -77,8 +77,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_SELINUX_STATUS = "selinux_status";
     private static final String KEY_BASEBAND_VERSION = "baseband_version";
     private static final String KEY_FIRMWARE_VERSION = "firmware_version";
-    private static final String KEY_SLIM_VERSION = "slim_version";
-    private static final String KEY_SLIM_BUILD_DATE = "build_date";
+    private static final String KEY_TIPSY_VERSION = "tipsy_version";
+    private static final String KEY_TIPSY_BUILD_DATE = "build_date";
     private static final String KEY_EQUIPMENT_ID = "fcc_equipment_id";
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
@@ -100,8 +100,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
 
         setStringSummary(KEY_FIRMWARE_VERSION, Build.VERSION.RELEASE);
         findPreference(KEY_FIRMWARE_VERSION).setEnabled(true);
-        setValueSummary(KEY_SLIM_VERSION, "ro.slim.version");
-        setValueSummary(KEY_SLIM_BUILD_DATE, "ro.build.date");
+        setValueSummary(KEY_TIPSY_VERSION, "ro.tipsy.version");
+        setValueSummary(KEY_TIPSY_BUILD_DATE, "ro.build.date");
         setValueSummary(KEY_BASEBAND_VERSION, "gsm.version.baseband");
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL + getMsvSuffix());
         setValueSummary(KEY_EQUIPMENT_ID, PROPERTY_EQUIPMENT_ID);
