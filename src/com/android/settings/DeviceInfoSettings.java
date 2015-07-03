635fb2176bf2af44b6950ad8b424e27913c19c80
martinusbe <martinusbe@gmail.com>
2015-07-03 09:21:58 +0200

    Revert "Settings: Show Baseband version on MSIM devices"
    This reverts commit 1fa28dc1915078fd9cbe0e94e33d3a36b60b23c9.

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -143,9 +143,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                 PROPERTY_EQUIPMENT_ID);
 
         // Remove Baseband version if wifi-only device
-        if ((Utils.isWifiOnly(getActivity())
-                || (TelephonyManager.getDefault().getPhoneCount() > 1))
-                && !Utils.showSimCardTile(getActivity())) {
+        if (Utils.isWifiOnly(getActivity())
+                || (TelephonyManager.getDefault().getPhoneCount() > 1)) {
             getPreferenceScreen().removePreference(findPreference(KEY_BASEBAND_VERSION));
         }
 
@@ -409,7 +408,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                     keys.add(KEY_EQUIPMENT_ID);
                 }
                 // Remove Baseband version if wifi-only device
-                if (Utils.isWifiOnly(context) && !Utils.showSimCardTile(context)) {
+                if (Utils.isWifiOnly(context)) {
                     keys.add((KEY_BASEBAND_VERSION));
                 }
                 // Dont show feedback option if there is no reporter.
