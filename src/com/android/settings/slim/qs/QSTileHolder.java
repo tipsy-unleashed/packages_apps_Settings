9907128c24acd3470f43c6c346e4222d0580c64d
existancevip <existancevip@gmail.com>
2015-07-06 19:46:47 -0400

    QS Floating tile
    code by rogersb11
    
    Signed-off-by: martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	res/values/tesla_strings.xml
    	res/xml/recent_panel_settings.xml
    	src/com/android/settings/slim/qs/QSTileHolder.java

--- src/com/android/settings/slim/qs/QSTileHolder.java
+++ src/com/android/settings/slim/qs/QSTileHolder.java
@@ -161,6 +161,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_expanded_desktop";
                 stringId = R.string.qs_expanded_desktop_tile;
                 break;
+            case QSConstants.TILE_SLIM_FLOATS:
+                resourceName = "ic_qs_floating_on";
+                stringId = R.string.qs_tile_slim_floats;
+                break;
             default:
                 return null;
         }
