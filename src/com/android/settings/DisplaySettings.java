a030a155ce7f6b9ae5b426a5b1c62e82a5b42fd6
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-08-14 23:40:58 +0200

    Settings: fix CMHW tap to wake setting
    Correctly set value on boot.
    
    Change-Id: I250efb61308f1898bc6738e14dc7797fba6deea0
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -275,6 +275,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (advancedPrefs != null && !mCmHardwareManager.isSupported(FEATURE_TAP_TO_WAKE)) {
             prefSet.removePreference(advancedPrefs);
             mTapToWake = null;
+        } else {
+            if (mTapToWake != null) {
+                mTapToWake.setChecked(mCmHardwareManager.get(FEATURE_TAP_TO_WAKE));
+            }
         }
 
         boolean proximityCheckOnWait = getResources().getBoolean(
@@ -455,9 +459,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     @Override
     public void onResume() {
         super.onResume();
-        if (mTapToWake != null) {
-            mTapToWake.setChecked(mCmHardwareManager.get(FEATURE_TAP_TO_WAKE));
-        }
         updateState();
         getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.ACCELEROMETER_ROTATION), true,
@@ -509,6 +510,12 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     com.android.internal.R.bool.config_doze_enabled_by_default) ? 1 : 0);
             mDozePreference.setChecked(value != 0);
         }
+
+        // Update tap-to-wake if it is available.
+        if (mTapToWake != null) {
+            mTapToWake.setChecked(mCmHardwareManager.get(FEATURE_TAP_TO_WAKE));
+        }
+
     }
 
     private void updateScreenSaverSummary() {
