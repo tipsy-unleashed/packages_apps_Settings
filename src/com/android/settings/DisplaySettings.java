3504ed13a392eac56722c152fd41d10ead301aa6
Juan Ezquerro LLanes <arrase@gamil.com>
2015-08-19 21:25:41 +0200

    [1/2] Settings: Add lockscreen rotation as an optional rotation setting
    Taken fom:
    
    https://github.com/gokussjx/cm_packages_apps_Settings/commit/a83ae47e184a778ca3067416c56901da70c1bc27
    
    PS7
    fixed value handling
    PS8
    Fix option doesn't stick
    
    AICPfy
    
    Change-Id: I3fd1d2c31d83230d480a0027cb7bf5fcf2db5cb7
    Signed-off-by: martinusbe <martinusbe@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -97,7 +97,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_SCREEN_SAVER = "screensaver";
     private static final String KEY_LIFT_TO_WAKE = "lift_to_wake";
     private static final String KEY_AUTO_BRIGHTNESS = "auto_brightness";
-    private static final String KEY_AUTO_ROTATE = "auto_rotate";
     private static final String KEY_WAKEUP_WHEN_PLUGGED_UNPLUGGED = "wakeup_when_plugged_unplugged";
     private static final String KEY_WAKEUP_CATEGORY = "category_wakeup_options";
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
@@ -369,10 +368,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     }
 
     private void updateDisplayRotationPreferenceDescription() {
-        if (mDisplayRotationPreference == null) {
+        PreferenceScreen preference = mDisplayRotationPreference;
+        if (preference == null) {
             return;
         }
-        PreferenceScreen preference = mDisplayRotationPreference;
+        preference.setEnabled(RotationPolicy.isRotationLockToggleVisible(getActivity()));
         StringBuilder summary = new StringBuilder();
         Boolean rotationEnabled = Settings.System.getInt(getContentResolver(),
                 Settings.System.ACCELEROMETER_ROTATION, 0) != 0;
@@ -714,9 +714,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     if (!isDozeAvailable(context)) {
                         result.add(KEY_DOZE);
                         result.add(KEY_ADVANCED_DOZE_OPTIONS);
-                    }
-                    if (!RotationPolicy.isRotationLockToggleVisible(context)) {
-                        result.add(KEY_AUTO_ROTATE);
                     }
                     return result;
                 }
